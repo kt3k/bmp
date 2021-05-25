ea0d07a6e435fade0b086e70eb4e785d71a0a57a
Yoshiya Hinosawa <stibium121@gmail.com>
2021-05-25 22:19:41 +0900

    refactor: separate models

---
+++ models_test.ts
@@ -0,0 +1,78 @@
+import {
+  assertEquals,
+  assertThrowsAsync,
+} from "https://deno.land/std@0.97.0/testing/asserts.ts";
+
+import { AppError, Version, VersionInfo } from "./models.ts";
+
+Deno.test("Version.parse - parses version number", () => {
+  let v = Version.parse("1.2.3");
+  assertEquals(v.major, 1);
+  assertEquals(v.minor, 2);
+  assertEquals(v.patch, 3);
+  assertEquals(v.preid, undefined);
+
+  v = Version.parse("2.3.4-alpha.1");
+  assertEquals(v.major, 2);
+  assertEquals(v.minor, 3);
+  assertEquals(v.patch, 4);
+  assertEquals(v.preid, "alpha.1");
+});
+
+Deno.test("Version - updates version number", () => {
+  let v = Version.parse("1.2.3");
+  v = v.bumpMajor();
+  assertEquals(v.major, 2);
+  assertEquals(v.minor, 2);
+  assertEquals(v.patch, 3);
+  assertEquals(v.preid, undefined);
+  v = v.bumpMinor();
+  assertEquals(v.major, 2);
+  assertEquals(v.minor, 3);
+  assertEquals(v.patch, 3);
+  assertEquals(v.preid, undefined);
+  v = v.bumpPatch();
+  assertEquals(v.major, 2);
+  assertEquals(v.minor, 3);
+  assertEquals(v.patch, 4);
+  assertEquals(v.preid, undefined);
+  v = v.setPreid("alpha.1");
+  assertEquals(v.major, 2);
+  assertEquals(v.minor, 3);
+  assertEquals(v.patch, 4);
+  assertEquals(v.preid, "alpha.1");
+  v = v.release();
+  assertEquals(v.major, 2);
+  assertEquals(v.minor, 3);
+  assertEquals(v.patch, 4);
+  assertEquals(v.preid, undefined);
+});
+
+Deno.test("VersionInfo.create", async () => {
+  await assertThrowsAsync(
+    async () => {
+      VersionInfo.create({});
+    },
+    AppError,
+    "Error: version property is not given in the config file",
+  );
+});
+
+Deno.test("VersionInfo - toObject", async () => {
+  const info = VersionInfo.create({
+    version: "1.2.3",
+    commit: "chore: bump to %.%.%",
+    files: {
+      "README.md": ["v%.%.%", "@%.%.%"],
+      "main.ts": [`"%.%.%"`],
+    }
+  });
+  assertEquals(info.toObject(), {
+    version: "1.2.3",
+    commit: 'chore: bump to %.%.%',
+    files: {
+      "README.md": ["v%.%.%", '@%.%.%'],
+      "main.ts": '"%.%.%"'
+    }
+  });
+});
--- read_config_test.ts
+++ read_config_test.ts
@@ -2,7 +2,8 @@ import {
   assertEquals,
   assertThrowsAsync,
 } from "https://deno.land/std@0.97.0/testing/asserts.ts";
-import { AppError, readConfig, Version, VersionInfo } from "./read_config.ts";
+import { readConfig } from "./read_config.ts";
+import { AppError } from "./models.ts";
 
 Deno.test("readConfig - throws when the config file is not found", async () => {
   await assertThrowsAsync(
@@ -24,76 +25,4 @@ Deno.test("readConfig - throws when the config file has syntax error", async () 
     AppError,
     "end of the stream or a document separator is expected at line 2, column 6:",
   );
-});
-
-Deno.test("Version.parse - parses version number", () => {
-  let v = Version.parse("1.2.3");
-  assertEquals(v.major, 1);
-  assertEquals(v.minor, 2);
-  assertEquals(v.patch, 3);
-  assertEquals(v.preid, undefined);
-
-  v = Version.parse("2.3.4-alpha.1");
-  assertEquals(v.major, 2);
-  assertEquals(v.minor, 3);
-  assertEquals(v.patch, 4);
-  assertEquals(v.preid, "alpha.1");
-});
-
-Deno.test("Version - updates version number", () => {
-  let v = Version.parse("1.2.3");
-  v = v.bumpMajor();
-  assertEquals(v.major, 2);
-  assertEquals(v.minor, 2);
-  assertEquals(v.patch, 3);
-  assertEquals(v.preid, undefined);
-  v = v.bumpMinor();
-  assertEquals(v.major, 2);
-  assertEquals(v.minor, 3);
-  assertEquals(v.patch, 3);
-  assertEquals(v.preid, undefined);
-  v = v.bumpPatch();
-  assertEquals(v.major, 2);
-  assertEquals(v.minor, 3);
-  assertEquals(v.patch, 4);
-  assertEquals(v.preid, undefined);
-  v = v.setPreid("alpha.1");
-  assertEquals(v.major, 2);
-  assertEquals(v.minor, 3);
-  assertEquals(v.patch, 4);
-  assertEquals(v.preid, "alpha.1");
-  v = v.release();
-  assertEquals(v.major, 2);
-  assertEquals(v.minor, 3);
-  assertEquals(v.patch, 4);
-  assertEquals(v.preid, undefined);
-});
-
-Deno.test("VersionInfo.create", async () => {
-  await assertThrowsAsync(
-    async () => {
-      VersionInfo.create({});
-    },
-    AppError,
-    "Error: version property is not given in the config file",
-  );
-});
-
-Deno.test("VersionInfo - toYaml", async () => {
-  const info = VersionInfo.create({
-    version: "1.2.3",
-    commit: "chore: bump to %.%.%",
-    files: {
-      "README.md": ["v%.%.%", "@%.%.%"],
-      "main.ts": [`"%.%.%"`],
-    }
-  });
-  assertEquals(info.toYaml(), `version: 1.2.3
-commit: 'chore: bump to %.%.%'
-files:
-  README.md:
-    - v%.%.%
-    - '@%.%.%'
-  main.ts: '"%.%.%"'
-`);
 });
