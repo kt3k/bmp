545dc81874ec3e7ed12c20cc654f3bd17969dbb3
Yoshiya Hinosawa <stibium121@gmail.com>
2024-08-21 10:51:12 +0900

    fix: lint (#4)

--- cli_test.ts
+++ cli_test.ts
@@ -3,37 +3,23 @@ import { assertEquals, assertStringIncludes } from "@std/assert";
 const decoder = new TextDecoder();
 
 Deno.test("cli --help", async () => {
-  const p = Deno.run({
-    cmd: [Deno.execPath(), "run", "cli.ts", "--help"],
-    stdout: "piped",
-    stderr: "piped",
+  const p = new Deno.Command(Deno.execPath(), {
+    args: ["run", "cli.ts", "--help"],
   });
 
-  const [status, output, _stderrOutput] = await Promise.all([
-    p.status(),
-    p.output(),
-    p.stderrOutput(),
-  ]);
+  const { code, stdout } = await p.output();
 
-  assertEquals(status.code, 0);
-  assertStringIncludes(decoder.decode(output), "Usage:");
-  p.close();
+  assertEquals(code, 0);
+  assertStringIncludes(decoder.decode(stdout), "Usage:");
 });
 
 Deno.test("cli --version", async () => {
-  const p = Deno.run({
-    cmd: [Deno.execPath(), "run", "cli.ts", "--version"],
-    stdout: "piped",
-    stderr: "piped",
+  const p = new Deno.Command(Deno.execPath(), {
+    args: ["run", "cli.ts", "--version"],
   });
 
-  const [status, output, _stderrOutput] = await Promise.all([
-    p.status(),
-    p.output(),
-    p.stderrOutput(),
-  ]);
+  const { code, stdout } = await p.output();
 
-  assertEquals(status.code, 0);
-  assertStringIncludes(decoder.decode(output), "bmp@");
-  p.close();
+  assertEquals(code, 0);
+  assertStringIncludes(decoder.decode(stdout), "bmp@");
 });
--- perform_commit.ts
+++ perform_commit.ts
@@ -1,17 +1,29 @@
 import { green } from "@std/fmt/colors";
-import { VersionInfo } from "./models.ts";
+import type { VersionInfo } from "./models.ts";
 
 export async function performCommit(info: VersionInfo) {
   console.log("====> Executing git commands");
   console.log(green(`git add .`));
-  const p0 = Deno.run({ cmd: ["git", "add", "."] });
-  await p0.status();
+  const p0 = new Deno.Command("git", {
+    args: ["add", "."],
+    stdout: "inherit",
+    stderr: "inherit",
+  });
+  await p0.output();
   const m = info.getCommitMessage();
   console.log(green(`git commit -m "${m}"`));
-  const p1 = Deno.run({ cmd: ["git", "commit", "-m", m] });
-  await p1.status();
+  const p1 = new Deno.Command("git", {
+    args: ["commit", "-m", m],
+    stdout: "inherit",
+    stderr: "inherit",
+  });
+  await p1.output();
   const t = info.getTag();
   console.log(green(`git tag ${t}`));
-  const p2 = Deno.run({ cmd: ["git", "tag", t] });
-  await p2.status();
+  const p2 = new Deno.Command("git", {
+    args: ["tag", t],
+    stdout: "inherit",
+    stderr: "inherit",
+  });
+  await p2.output();
 }
